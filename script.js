14f37bf12fa7d90a85f043b4e334e8281c03ab1d
Mehjbeen <mehjbeen22@navgurukul.org>
2023-08-07 12:53:10 +0530

    fetch processing is done

---
+++ script.js
@@ -0,0 +1,30 @@
+let textInput = document.getElementById("txtInput");
+let Translatebtn = document.getElementById("Translator");
+let OutputDiv = document.querySelector("#txtTranslated");
+
+// OutputDiv.innerHTML ="hello Mehjbeen"
+
+let serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+  console.log("error occurred", error);
+  alert("something went wrong");
+}
+
+function clickHandler() {
+  let InputText = textInput.value;
+
+  fetch(getTranslationURL(InputText))
+    .then((response) => response.json())
+    .then((json) => {
+      let translatedText = json.contents.translated;
+      OutputDiv.innerHTML = translatedText;
+    })
+    .catch(errorHandler);
+}
+
+Translatebtn.addEventListener("click", clickHandler);
